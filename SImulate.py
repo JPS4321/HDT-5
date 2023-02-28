d51786abf2cce98886735cfc9217e1c95b7973d6
JPS4321 <104738904+JPS4321@users.noreply.github.com>
2023-02-28 17:01:29 -0600

    Add files via upload

--- SImulate.py
+++ SImulate.py
@@ -1,37 +1,17 @@
 import simpy
 import random as rand
 
+Cantidad_Procesos = 25
+Intervalos = 10
+Solicitud_Memoria = rand.expovariate(1.0/Intervalos)
+Cantidad_Memoria_RAM = 100
+Capacidad = 1
 
 
-
-
-class Sistema():
-    def __init__(self,Cantidad_Ram,Cantidad_Instrucciones):
-        self.Cantidad_Ram = Cantidad_Ram
-        self.Cantidad_Instrucciones = Cantidad_Instrucciones
-        
-class Proceso:
-    def __init__(self):
-        self.Numero_Instrucciones = rand.randint(1,10)
-        self.Espacio_Memoria = rand.randint(1,10)
-
-
-def run(env,Procesos,Sistema):
-    print("RAM incial: " + str(Sistema.Cantidad_Ram))
-    while True:
-        print("Ha llegado un nuevo proceso en " + str(env.now))
-        ProcesoNuevo = Proceso()
-        print("Cantidad de instrucciones: "+str(ProcesoNuevo.Numero_Instrucciones))
-        print("Espacio a utilizar: "+str(ProcesoNuevo.Espacio_Memoria))
-        print("Cantidad de Ram actual: "+str(Sistema.Cantidad_Ram))
-
-        
-
-
-hola = Sistema(100,10)
-env = simpy.Environment()
-env.process(run(env,10,hola))
-
-        
-#KOU
-#Esteban 
+class Computadora:
+    def __init__(self, env, Cantidad_Procesos, Intervalos, Cantidad_Memoria_RAM, Capacidad):
+        self.env = env
+        self.Cantidad_Procesos = Cantidad_Procesos
+        self.Intervalos = Intervalos
+        self.Cantidad_Memoria_RAM = Cantidad_Memoria_RAM
+        self.Capacidad = simpy.Resource(env,Capacidad)
