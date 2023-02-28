4ed2f3fbbbcf1da8626d89a6d94e4dda61c83bd1
JPS4321 <sol22102@uvg.edu.gt>
2023-02-27 22:25:15 -0600

    Update SImulate.py

--- SImulate.py
+++ SImulate.py
@@ -1,11 +1,14 @@
 import simpy
 import random as rand
 
-RAMS = 100
 
-class RAM:
-    def __init__(self,Cantidad_Ram):
-        self.Cantidad_Ram = RAMS
+
+
+
+class Sistema():
+    def __init__(self,Cantidad_Ram,Cantidad_Instrucciones):
+        self.Cantidad_Ram = Cantidad_Ram
+        self.Cantidad_Instrucciones = Cantidad_Instrucciones
         
 class Proceso:
     def __init__(self):
@@ -13,8 +16,22 @@ class Proceso:
         self.Espacio_Memoria = rand.randint(1,10)
 
 
-
+def run(env,Procesos,Sistema):
+    print("RAM incial: " + str(Sistema.Cantidad_Ram))
+    while True:
+        print("Ha llegado un nuevo proceso en " + str(env.now))
+        ProcesoNuevo = Proceso()
+        print("Cantidad de instrucciones: "+str(ProcesoNuevo.Numero_Instrucciones))
+        print("Espacio a utilizar: "+str(ProcesoNuevo.Espacio_Memoria))
+        print("Cantidad de Ram actual: "+str(Sistema.Cantidad_Ram))
 
         
-##KOU UWU
-#Esteban UwU*
+
+
+hola = Sistema(100,10)
+env = simpy.Environment()
+env.process(run(env,10,hola))
+
+        
+#KOU
+#Esteban 
