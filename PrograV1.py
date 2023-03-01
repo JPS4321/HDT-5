85ea40b678d32aaf56a76c48c1245742809cec32
EstebanZG999 <zam22119@uvg.edu.gt>
2023-02-28 18:43:35 -0600

    Media, desviacion

--- PrograV1.py
+++ PrograV1.py
@@ -1,5 +1,7 @@
 import simpy
 import random
+import numpy as np
+import statistics
 
 RAM_TOTAL = 100
 InstruccionesPorCiclo = 3
@@ -10,6 +12,8 @@ ID = 0
 Intervalo = 10
 generacion_Pro_Azar = 0
 Tiempo_Fuera = 0.5
+desviacion = []
+media = []
 
 class Proceso():
     def __init__(self, env, Cantidad_Instrucciones, Cantidad_Espacio,ID):
@@ -29,6 +33,7 @@ class Proceso():
         print("Este proceso tiene: "+str(InstruccionAzar)+" instrucciones")
 
 def Iniciar(env, Pro,RAM):
+    global times
     yield env.process(Pro.prueba())
     if Pro.Cantidad_Espacio < RAM:
         RAM = RAM - Pro.Cantidad_Espacio
@@ -38,6 +43,7 @@ def Iniciar(env, Pro,RAM):
         Pro.Cantidad_Instrucciones = Pro.Cantidad_Instrucciones - InstruccionesPorCiclo
         yield env.timeout(Tiempo_Fuera)
         if Pro.Cantidad_Instrucciones <= 0:
+            media.append(env.now)
             print("El proceso ha sido procesado con exito")
             print("Tiempo de salida: "+str(env.now))
             break
@@ -46,17 +52,23 @@ def Iniciar(env, Pro,RAM):
 
 
 
-
 env = simpy.Environment()
-for i in range(25):
+for i in range(50):
     generacion_Pro_Azar = random.expovariate(1.0 / Intervalo)
     InstruccionAzar = random.randint(1,10)
     EspacioAzar = random.randint(1,10)
     Pro = Proceso(env,InstruccionAzar,EspacioAzar,ID)
     env.process(Iniciar(env,Pro,RAM_TOTAL))
     env.run()
+    process_time = env.now - generacion_Pro_Azar
+    desviacion.append(process_time)
     ID= ID+1
     
 
 
-        
+std_dev = np.std(desviacion)
+print('\n')
+print("La media de los tiempos de finalización es: ", statistics.mean(media))
+print('\n')
+print("Desviacion estandar de las corridas:", std_dev) 
+print('\n')
